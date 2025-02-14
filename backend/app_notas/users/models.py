4c1f4a1eef476c5f78a2514b982107fa98c440ec
juan garcia <juanjose10312@gmail.com>
2025-02-14 13:20:00 -0700

    backend

---
+++ backend/app_notas/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from  django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser): 
+    email = models.EmailField(unique=True) # Email unico para cada usuario
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
