e5fc36cb41c7c6e6a038b5d83a08acc4f4b46d13
juan garcia <juanjose10312@gmail.com>
2025-02-15 14:10:06 -0700

    hosts

--- backend/app_notas/app_notas/settings.py
+++ backend/app_notas/app_notas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--vx1ouw)*50)2n6(nd!o33e9ob^m8+9)ikfsw91o0x+#z_08!*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
