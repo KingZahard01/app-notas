1b890222fc28df93d0bf540f0757084c413463e9
juan garcia <juanjose10312@gmail.com>
2025-02-15 14:36:08 -0700

    add url

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://app-notas-backend-1.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
